505b23974b5f2cda09f872f713859a0256640207
Krislette <acellekrislette@gmail.com>
2024-06-03 15:15:38 +0800

    Initial commit

---
+++ src/frontend/Main.java
@@ -0,0 +1,11 @@
+package frontend;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Application().setVisible(true));
+    }
+    
+}
